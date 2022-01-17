eae84e5c4e928618ed6c199b8217e72adcb09aea
brunoc <brunopls@protonmail.com>
2022-01-17 21:51:32 +0000

    Registered the controllers in service configuration
    Forgot to do this earlier

--- protostream/protostream/Startup.cs
+++ protostream/protostream/Startup.cs
@@ -25,6 +25,7 @@ namespace protostream
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddEntityFrameworkSqlite().AddDbContext<MovieContext>();
         }
 
