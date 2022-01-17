5240b6d132cfa1827fbe5e8ffa04e59b8c6370e7
brunoc <brunopls@protonmail.com>
2022-01-17 21:58:21 +0000

    Added Get All endpoint w/ filtering and very basic ordering

--- protostream/protostream/Controllers/MovieController.cs
+++ protostream/protostream/Controllers/MovieController.cs
@@ -23,5 +23,33 @@ namespace protostream.Controllers
             var movie = _dbContext.Movies.Single(x => x.id == id);
             return movie;
         }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get([FromQuery(Name = "filter")] String? filter, [FromQuery(Name = "order")] string? order)
+        {
+            var movies = from m in _dbContext.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(filter))
+            {
+                movies = movies.Where(x => x.film.ToLower().Contains(filter.ToLower()));
+            }
+
+            if (!String.IsNullOrEmpty(order))
+            {
+                switch (order)
+                {
+                    case "year":
+                        movies = movies.OrderBy(x => x.year);
+                        break;
+                    case "audienceScore":
+                        movies = movies.OrderBy(x => x.audienceScore);
+                        break;
+                }
+            }
+
+
+            return movies.AsEnumerable<Movie>();
+        }
     }
 }
