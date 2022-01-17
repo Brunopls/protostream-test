b87cd04f4425b63cc58154929cd813debb1034d6
brunoc <brunopls@protonmail.com>
2022-01-17 21:48:47 +0000

    Created controllers class and added first method
    * Get by ID
    
    Async might've been a better option, but for the purpose of this test I will keep things this way.

---
+++ protostream/protostream/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using protostream.Models;
+
+namespace protostream.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MovieController : Controller
+    {
+        private readonly MovieContext _dbContext;
+        public MovieController(MovieContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("{id}")]
+        public Movie Get(int id)
+        {
+            var movie = _dbContext.Movies.Single(x => x.id == id);
+            return movie;
+        }
+    }
+}
