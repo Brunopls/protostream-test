2078b1a4af51f32dea5e3039934efba9e24e91bb
brunoc <brunopls@protonmail.com>
2022-01-17 21:22:16 +0000

    Initial migration

---
+++ protostream/protostream/Migrations/20220117212137_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace protostream.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    film = table.Column<string>(nullable: true),
+                    genre = table.Column<string>(nullable: true),
+                    leadStudio = table.Column<string>(nullable: true),
+                    audienceScore = table.Column<byte>(nullable: false),
+                    profitability = table.Column<decimal>(nullable: false),
+                    rottenTomatoes = table.Column<byte>(nullable: false),
+                    worldwideGross = table.Column<decimal>(nullable: false),
+                    year = table.Column<ushort>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
