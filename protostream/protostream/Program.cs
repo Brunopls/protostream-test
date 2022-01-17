de37816328d17dd60f047547bd10c40b429f7644
brunoc <brunopls@protonmail.com>
2022-01-17 21:44:31 +0000

    Added code to populate database with values extracted from CSV file
    There is probably a better way to go about this.
    
    Essentially the database just gets filled with data pulled from the CSV file, which is parsed and mapped against the model I created earlier.

--- protostream/protostream/Program.cs
+++ protostream/protostream/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using protostream.Models;
 
 namespace protostream
 {
@@ -13,6 +15,38 @@ namespace protostream
     {
         public static void Main(string[] args)
         {
+            using (var db = new MovieContext())
+            {
+                if (!db.Set<Movie>().Any())
+                {
+                    using WebClient wc = new WebClient();
+                    string csvData = wc.DownloadString("https://gist.githubusercontent.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea/raw/0c794a9717f18b094eabab2cd6a6b9a226903577/movies.csv");
+                    List<Movie> movies = new List<Movie>();
+                    var lines = csvData.Split('\n');
+
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+
+                        string[] csvRow = lines[i].Split(',');
+                        csvRow[6] = csvRow[6].Remove(0, 1);
+
+                        movies.Add(new Movie
+                        {
+                            id = i,
+                            film = csvRow[0],
+                            genre = csvRow[1],
+                            leadStudio = csvRow[2],
+                            audienceScore = Convert.ToByte(csvRow[3]),
+                            profitability = Convert.ToDecimal(csvRow[4]),
+                            rottenTomatoes = Convert.ToByte(csvRow[5]),
+                            worldwideGross = Convert.ToDecimal(csvRow[6]),
+                            year = Convert.ToUInt16(csvRow[7])
+                        });
+                    }
+                    db.AddRange(movies.ToArray());
+                    db.SaveChanges();
+                }
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
